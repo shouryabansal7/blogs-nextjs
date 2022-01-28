62b17bd30ab1de6f0a7220959eab22ea9a5624b1
Shourya Bansal <shouryab2001@gmail.com>
2022-01-29 02:54:29 +0530

    fourth commit : create contact form and store messages in mongodb via api

---
+++ pages/api/contact.js
@@ -0,0 +1,59 @@
+import { MongoClient } from "mongodb";
+// l3Wl7WEnN67gsz6L;
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const { name, email, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      return res.status(422).json({
+        message: "Invalid input",
+      });
+    }
+
+    //store it in database
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://admin:l3Wl7WEnN67gsz6L@cluster0.gxrsv.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
+
+    res.status(201).json({
+      message: "Successfully Stored Message!",
+      newMessage,
+    });
+  }
+}
+
+export default handler;
