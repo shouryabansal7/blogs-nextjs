a1dccf16c684b166f5adeb8594de5f1f79aeb984
Shourya Bansal <shouryab2001@gmail.com>
2022-01-29 03:38:32 +0530

    sixth commit : write meta data and create _document file

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
@@ -6,6 +7,13 @@ import { getFeaturedPosts } from "../lib/posts-utils";
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Max' Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
